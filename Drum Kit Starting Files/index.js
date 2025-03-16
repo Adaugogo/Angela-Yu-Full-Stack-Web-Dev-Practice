1ac5cae6e0146a1d37d0c93f4380cf37706b2dc9
Adaugo Ukaumunna <106121642+Adaugogo@users.noreply.github.com>
2025-03-16 03:24:07 +0100

    Built a drum kit using JS DOM manipulation. Yayy!

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,73 @@
+var buttons=document.querySelectorAll('button.drum');
+for (let i=0; i<buttons.length; i++ ){
+   buttons[i].addEventListener('click', handleClick)
+}
+
+
+//  Keyboard Press
+function handleClick(){
+    // the button that triggered the event is stored in a variable that changes everytime a new button is pressed.
+    var buttonInnerHTML=this.innerHTML;
+    makeSound(buttonInnerHTML)
+    buttonAnimation(buttonInnerHTML)
+   
+    }
+
+//  Keyboard Press
+document.addEventListener('keypress', function(event) {
+   var theKey= event.key;
+   makeSound(theKey)
+   buttonAnimation(theKey)
+})
+
+function makeSound(key){
+
+    switch(key){
+        case 'w':
+            var tom1 = new Audio ("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        
+        case 'a':
+            var tom2 = new Audio ("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case 's':
+            var tom3 = new Audio ("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case 'd':
+            var tom4 = new Audio ("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case 'j':
+            var crash = new Audio ("./sounds/crash.mp3");
+            crash.play();
+            break;   
+
+        case 'k':
+            var kick = new Audio ("./sounds/kick-bass.mp3");
+            kick.play();
+            break;   
+    
+        case 'l':
+        var snare = new Audio ("./sounds/snare.mp3");
+        snare.play();
+        break;   
+}
+}
+function buttonAnimation (currentkey){
+    var keypressed= document.querySelector('.' + currentkey)
+    keypressed.classList.add('pressed');
+
+    setTimeout (function() {
+        keypressed.classList.remove('pressed'); },
+        200 )  
+   
+}
+
+
+ 
